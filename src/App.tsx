c870b47b6ac315546592ac461c814ccd89276050
HUN <154392026+xaexunxang@users.noreply.github.com>
2024-07-10 03:06:34 +0900

    Erase CategoryPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
+import CategoryPage from './pages/CategoryPage';
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/category" element={<CategoryPage />}/>
     </Routes>
   );
 }
