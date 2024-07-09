6ad8db1135faa83c05287917530be553d671b711
UpToMind <114003042+UpToMind@users.noreply.github.com>
2024-07-10 00:24:26 +0900

    Onboarding Page1 Complete (#14)
    Co-authored-by: 이동준 <idongjun@idongjun-ui-MacBookPro.local>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Noto Sans', 'sans-serif'],
+      },
+      backgroundImage: {
+        'custom-image': "url('./public/main_solomon.png')", // 경로를 실제 이미지 파일 위치로 수정하세요
+      },
+      backgroundPosition: {
+        'center-bottom': 'center bottom', // 커스텀 배경 위치 추가
+      },
+    },
   },
   plugins: [],
 }
