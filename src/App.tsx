e0ff178c5350d2aef50b8b2f5cb82b33912c8b38
Sumin Lee <144830946+Sum1nLee@users.noreply.github.com>
2024-07-12 11:19:14 +0900

    [feat/#12] 재판장 페이지 퍼블리싱 완료  (#31)
    * 결과페이지 생성
    
    * feat: 재판장 페이지 퍼블리싱 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
+import JudgePage from './pages/JudgePage';
 import CategoryPage from './pages/CategoryPage';
-
 import LoginPage from './pages/LoginPage';
 import MainPage2 from './pages/MainPage2';
 import SignupPage from './pages/SignupPage';
@@ -10,6 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/judgepage" element={<JudgePage />} />
       <Route path="/CategoryPage" element={<CategoryPage />}/>
       <Route path="/Login" element={<LoginPage />} />
       <Route path="/MainPage2" element={<MainPage2 />} />
