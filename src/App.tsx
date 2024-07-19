8d043b1488543b765d14ac2dfbd10d73c0c19ec5
HUN <154392026+xaexunxang@users.noreply.github.com>
2024-07-20 00:50:02 +0900

    카테고리 테스트 페이지 삭제 및 App.tsx 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,26 @@
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
-import CategoryPage from './pages/CategoryPage';
-
-import CategoryMapTest from './pages/categoruMaptest';
-
+import MainPage2 from './pages/MainPage2';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
-import ResultPage from './pages/ResultPage';
-import MainPage2 from './pages/MainPage2';
-import LatestPostPage from './pages/LatestPostPage';
-import MyPostPage from './pages/MyPostPage';
+import CategoryPage from './pages/CategoryPage';
 import JudgePageCopy from './pages/JudgePageCopy';
 import JudgePageCopy2 from './pages/JudgePageCopy2';
+import ResultPage from './pages/ResultPage';
+import LatestPostPage from './pages/LatestPostPage';
+import MyPostPage from './pages/MyPostPage';
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/MainPage2" element={<MainPage2 />} />
+      <Route path="/Login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/CategoryPage" element={<CategoryPage />} />
       <Route path="/JudgePageCopy" element={<JudgePageCopy />} />
       <Route path="/JudgePageCopy2" element={<JudgePageCopy2 />} />
-      <Route path="/CategoryPage" element={<CategoryPage />} />
-
-      <Route path="/CategoryMapTest" element={<CategoryMapTest />} />
-
-      <Route path="/Login" element={<LoginPage />} />
-      <Route path="/MainPage2" element={<MainPage2 />} />
-      <Route path="/signup" element={<SignupPage />} />
       <Route path="/result" element={<ResultPage />} />
       <Route path="/LatestPostPage" element={<LatestPostPage />} />
       <Route path="/MyPostPage" element={<MyPostPage />} />
