877637b6ba3ecea8c3a1556c65975751c3100783
HUN <154392026+xaexunxang@users.noreply.github.com>
2024-07-20 00:43:01 +0900

    게시판 카테고리 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import CategoryPage from './pages/CategoryPage';
 
+import CategoryMapTest from './pages/categoruMaptest';
+
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import ResultPage from './pages/ResultPage';
@@ -18,6 +20,9 @@ function App() {
       <Route path="/JudgePageCopy" element={<JudgePageCopy />} />
       <Route path="/JudgePageCopy2" element={<JudgePageCopy2 />} />
       <Route path="/CategoryPage" element={<CategoryPage />} />
+
+      <Route path="/CategoryMapTest" element={<CategoryMapTest />} />
+
       <Route path="/Login" element={<LoginPage />} />
       <Route path="/MainPage2" element={<MainPage2 />} />
       <Route path="/signup" element={<SignupPage />} />
