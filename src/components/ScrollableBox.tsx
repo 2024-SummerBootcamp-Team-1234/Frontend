ea773342ece818120dc359e8a0965859e67d151b
이수민 <sammy1313@naver.com>
2024-07-22 16:50:43 +0900

    feat: 스크롤 자동으로 내려오기 구현

--- src/components/ScrollableBox.tsx
+++ src/components/ScrollableBox.tsx
@@ -1,21 +1,30 @@
+import React, { useEffect, useRef } from 'react';
+
 interface ScrollableBoxProps {
   content: string;
   className?: string;
 }
 
-// ScrollableBox라는 함수형 컴포넌트를 만듦. content를 속성으로 받음
 const ScrollableBox: React.FC<ScrollableBoxProps> = ({
   content,
   className,
 }) => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    }
+  }, [content]);
+
   return (
     <div
-      className={`pt-10 pb-10 pr-10 pl-10 rounded-[20px] text-white  overflow-y-auto ${className}`}
+      ref={scrollRef}
+      className={`pt-10 pb-10 pr-10 pl-10 rounded-[20px] text-white overflow-y-auto ${className}`}
     >
       {content}
     </div>
   );
 };
 
-// ScrollableBox 컴포넌트를 다른 파일에서 사용할 수 있도록 내보냄
 export default ScrollableBox;
