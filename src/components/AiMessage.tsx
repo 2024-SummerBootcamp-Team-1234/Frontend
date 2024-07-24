d7bd31afe200f528acc70f3087d609f416619b01
UpToMind <114003042+UpToMind@users.noreply.github.com>
2024-07-24 20:59:44 +0900

    feat : 채팅에서 post로 response 받아올 때 html태그 적용해서 보여주는 기능추가 (#90)
    Co-authored-by: 이동준 <idongjun@idongjun-ui-MacBookPro.local>

--- src/components/AiMessage.tsx
+++ src/components/AiMessage.tsx
@@ -1,12 +1,17 @@
 // AiMessage.tsx
 import React from 'react';
 import Profile from '../assets/Profile.png';
+import { marked } from 'marked';
 
 interface AiMessageProps {
   message: string;
 }
 
 const AiMessage: React.FC<AiMessageProps> = ({ message }) => {
+  const createMarkup = () => {
+    return { __html: marked(message) };
+  };
+
   return (
     <div className="flex items-center my-3">
       <img
@@ -15,7 +20,7 @@ const AiMessage: React.FC<AiMessageProps> = ({ message }) => {
         className="w-10 h-10 rounded-full mr-4 mb-7"
       />
       <div className="min-w-1/4 max-w-[60%] mb-3 flex-wrap bg-black p-4 rounded-2xl flex items-center border-2 border-white">
-        <p>{message}</p>
+        <p dangerouslySetInnerHTML={createMarkup()}></p>
       </div>
     </div>
   );
