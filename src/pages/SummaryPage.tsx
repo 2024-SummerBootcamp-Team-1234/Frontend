c4604417ed9cf8c3a6f87f17cedc300b37f61f0e
이동준 <idongjun@idongjun-ui-MacBookPro.local>
2024-07-28 00:19:52 +0900

    style : 자잘한 레이아웃 변경 버튼 위치를 판사의 채팅과 정렬 맞춤

--- src/pages/SummaryPage.tsx
+++ src/pages/SummaryPage.tsx
@@ -4,24 +4,32 @@ function SummaryPage() {
   return (
     <>
       <div className="flex w-screen h-screen bg-cover bg-center bg-summary-image">
-        <div className="flex-col w-[45%] h-[100%] p-4">
+        {/* 왼쪽 가로 45% 박스 */}
+        <div className="flex-col w-[45%] h-[100%] p-4 pb-16">
           <div className="w-[100%] h-[20%]"></div>
           <div className="flex w-[100%] h-[80%] items-end bg-contain bg-no-repeat bg-center-bottom bg-judge-image">
-            <div className="flex-col w-[100%] h-[30%] bg-contain bg-no-repeat bg-center bg-judgeChat-image"></div>
+            <div className="flex-col w-[100%] h-[30%] bg-contain bg-no-repeat bg-center-bottom bg-judgeChat-image"></div>
           </div>
         </div>
-        <div className="flex-col w-[60%] h-[100%] items-end p-12">
-          <div className="w-[100%] h-[10%] "></div>
-          <div className="w-[100%] h-[70%] bg-black rounded-2xl opacity-90 border-solid border-white border-2">
+
+        {/* 오른쪽 가로 55% 박스 */}
+        <div className="flex-col w-[55%] h-[100%] justify-end p-12 pb-16 pr-16">
+          {/* 위에 15% 박스 */}
+          <div className="w-[100%] h-[12%]"></div>
+
+          {/* 중간 75% 박스 */}
+          <div className="w-[100%] h-[75%] bg-black rounded-2xl opacity-90 border-solid border-white border-2">
             <div className="flex w-[100%] h-[100%]">
               <ScrollableBox
                 content="요약. 이게 판결이다.알겠냐"
-                className=" h-[100%] w-[100%] "
+                className="h-[100%] w-[100%]"
               />
             </div>
           </div>
-          <div className="flex-col w-[100%] h-[15%] content-center">
-            <button className="w-[100%] h-[60%] bg-black rounded-2xl opacity-90 border-solid border-white border-2 font-sans text-white">
+
+          {/* 하단 10% 박스 */}
+          <div className="flex-col w-[100%] h-[13%] content-end">
+            <button className="w-[100%] h-[60%] bg-black rounded-2xl opacity-90 border-solid border-white hover:bg-zinc-900 border-2 font-sans text-white text-[20px]">
               계속하기
             </button>
           </div>
